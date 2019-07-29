36e2062b13579659cf377d6e4f26085f2bac4b15
Raj Kadam <electic@users.noreply.github.com>
2019-07-28 17:28:52 -0700

    Fix typo.

--- node/apiClient.js
+++ node/apiClient.js
@@ -5,4 +5,4 @@ const axios = require("axios");
 /* Urls */
 const API_BASE_URL = "<your api url here>";
 const API_ADDRESS_URL = `${API_BASE_URL}/addresses/`;
-const API_TRANSACTIONS_URL = `${API_BASE_URL}/transations/`;
+const API_TRANSACTIONS_URL = `${API_BASE_URL}/transactions/`;
